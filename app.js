d3d56ed2ec6dece60f1e0d23cc3f50c126b7394a
officialmanishkr98 <officialmanishkr98@gmail.com>
2023-01-31 23:00:51 +0530

    multi-page website

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
+const header = document.querySelector('.header.container.service');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -12,7 +12,8 @@ document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
 		header.style.backgroundColor = '#29323c';
-	} else {
+	} 
+	else {
 		header.style.backgroundColor = 'transparent';
 	}
 });
